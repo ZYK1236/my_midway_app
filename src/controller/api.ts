3a8b5b6179b8b5b2d3bc6e7ccbd5c165c64d3e92
yykzhang <15986875679@163.com>
2022-01-14 12:14:34 +0800

    fix: network

--- src/controller/api.ts
+++ src/controller/api.ts
@@ -1,7 +1,8 @@
-import { Inject, Controller, Provide, Body, Post } from '@midwayjs/decorator';
+import { Inject, Controller, Provide, Body, Post, Get, Query } from '@midwayjs/decorator';
 import { Context } from 'egg';
-import { ILoginResponse } from '../interface';
+import { ILoginResponse, IGetUserResponse } from '../interface';
 import { LoginService } from '../service/login';
+import { UserService } from "../service/user"
 
 @Provide()
 @Controller('/api') // 用于进行路由前缀（分组），这样这个控制器下面的所有路由都会带上这个前缀。
@@ -11,10 +12,17 @@ export class APIController {
 
   @Inject()
   loginService: LoginService;
+  userService: UserService;
 
   @Post('/login')
   async Login(@Body() account: string, @Body() pwd: string): Promise<ILoginResponse> {
     const user = await this.loginService.getUser({ account, pwd });
     return { success: true, message: 'OK', data: user };
+  }
+
+  @Get('/get_user')
+  async GetUser(@Query() uid: string): Promise<IGetUserResponse> {
+    const user = await this.userService.getUser({ uid });
+    return { success: true, message: 'OK', data: user }
   }
 }
